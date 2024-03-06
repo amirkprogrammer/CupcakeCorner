87c406e659cb39543af43ccf050bcb362c46279c
amirkprogrammer <amir.m.kabiri@gmail.com>
2024-03-05 16:52:18 -0800

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Amir Kabiri on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
